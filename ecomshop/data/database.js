e45424e395945f0623bd1762764785def9caa62a
Brian <brianmichaeladero@gmail.com>
2023-12-16 20:43:21 +0300

    connection to a database

---
+++ ecomshop/data/database.js
@@ -0,0 +1,27 @@
+const mongodb = require("mongodb");
+const { get } = require("../routes/auth.routes");
+
+//create a mongodb client
+const MongoClient = mongodb.MongoClient;
+
+//connection to the database
+let database;
+
+async function connectToDatabase() {
+  const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+  database = client.db("online-shop");
+}
+
+//checking if we have the database
+function getDb() {
+  if (!darabase) {
+    throw new Error("You must connect first!!");
+  }
+
+  return database;
+}
+
+module.exports = {
+  connectToDatabase: connectToDatabase,
+  getDb: getDb,
+};
