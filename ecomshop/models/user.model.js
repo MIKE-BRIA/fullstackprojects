1862f3221478a6c57f4c97e2cd38c4e15197c890
Brian <brianmichaeladero@gmail.com>
2023-12-18 16:04:25 +0300

    collecting user data from signup page

---
+++ ecomshop/models/user.model.js
@@ -0,0 +1,33 @@
+//importing bcrpt for hashing password
+const bcrypt = require("bcryptjs");
+
+const db = require("../data/database");
+
+//this helps us collect user data
+class User {
+  constructor(email, password, fullname, street, postal, city) {
+    this.email = email;
+    this.password = password;
+    this.name = fullname;
+    this.address = {
+      street: street,
+      postalCode: postal,
+      city: city,
+    };
+  }
+
+  //store signup user data to the database
+  async signup() {
+    //hashing the password
+    const hashedPassword = await bcrypt.hash(this.password, 12);
+
+    await db.getDb().collection("users").insertOne({
+      email: this.email,
+      password: hashedPassword,
+      name: this.name,
+      address: this.address,
+    });
+  }
+}
+
+module.exports = User;
