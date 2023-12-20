90497bff565831c32ea9597a8941127455a0ecf4
Brian <brianmichaeladero@gmail.com>
2023-12-20 20:28:22 +0300

    authentication checker middleware

--- ecomshop/middlewares/check-auth.js
+++ ecomshop/middlewares/check-auth.js
@@ -1,4 +1,5 @@
 //middleware to check if user is authenticated to login
+// * checks also if user is admin or normal user
 
 function checkAuthStatus(req, res, next) {
   const uid = req.session.uid;
@@ -9,6 +10,7 @@ function checkAuthStatus(req, res, next) {
 
   res.locals.uid = uid;
   res.locals.isAuth = true;
+  res.locals.isAdmin = req.session.isAdmin;
   next();
 }
 
