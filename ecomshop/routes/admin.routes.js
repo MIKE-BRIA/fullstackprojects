a1f21cf86f333b1ae250f979d408885b684bb76e
Brian <brianmichaeladero@gmail.com>
2023-12-21 22:41:39 +0300

    routes handling admin pages

---
+++ ecomshop/routes/admin.routes.js
@@ -0,0 +1,22 @@
+// * routes for all admin routes
+
+//import express for use in routers
+const express = require("express");
+
+const adminController = require("../controllers/admin.controller");
+const imageUploadMiddleware = require("../middlewares/image-upload");
+
+const router = express.Router();
+
+//get route for product
+router.get("/products", adminController.getProducts); // ? /admin/products
+
+router.get("/products/new", adminController.getNewProduct); // ? /admin/products/new
+
+router.post(
+  "/products",
+  imageUploadMiddleware,
+  adminController.createNewProduct
+); //? extraction of uploaded image
+
+module.exports = router;
