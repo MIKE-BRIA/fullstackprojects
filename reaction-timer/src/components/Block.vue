ea58e3101d06b4ee40539ee51862c2b623180dd1
Brian <brianmichaeladero@gmail.com>
2024-01-06 18:22:03 +0300

    child- component(Display)

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock" @click="stopTimer">
     <h1>click me</h1>
   </div>
 </template>
@@ -7,6 +7,34 @@
 <script>
 export default {
   props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    console.log("component mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    //start timer function
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    //function that stop timer when the block is clicked
+    stopTimer() {
+      clearInterval(this.timer);
+      //emiting the reaction time to parent(App.vue)
+      this.$emit("end", this.reactionTime);
+    },
+  },
 };
 </script>
 
@@ -19,10 +47,5 @@ export default {
   text-align: center;
   padding: 100px;
   margin: 40px auto;
-}
-button {
-  padding: 1rem;
-  font-size: 1rem;
-  border-radius: 10px;
 }
 </style>
