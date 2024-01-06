9df68a66dae86836e726aa4d5dfcf3b9287f72e2
Brian <brianmichaeladero@gmail.com>
2024-01-06 14:16:40 +0300

    reaction-time

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Ninja Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay"></Block>
+  </div>
+</template>
+
+<script>
+import Block from "./components/Block";
+
+export default {
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 4000; //random number of seconds btwn 2seconds and 5seconds
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="block">
+    <h1>click me</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+};
+</script>
+
+<style>
+.block {
+  width: 15rem;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px;
+  margin: 40px auto;
+}
+button {
+  padding: 1rem;
+  font-size: 1rem;
+  border-radius: 10px;
+}
+</style>
