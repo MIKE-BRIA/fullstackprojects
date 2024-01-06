10361f996ad7190a5b8a926a0696cf34a726ac68
Brian <brianmichaeladero@gmail.com>
2024-01-06 18:22:56 +0300

    child- component(Results)

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,7 +1,34 @@
-<template></template>
+<template>
+  <div class="block">
+    <h1>Reaction Time : {{ score }} ms</h1>
+    <p>{{ rank }}</p>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  //prop from Parent (App.vue)
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 450) {
+      this.rank = "Ninja fingers";
+    } else if (this.score < 700) {
+      this.rank = "That's a good one";
+    } else {
+      this.rank = "Are you a slow thinker mate";
+    }
+  },
+};
 </script>
 
-<style></style>
+<style>
+p {
+  font-size: 1.7rem;
+  color: rgb(155, 33, 192);
+}
+</style>
