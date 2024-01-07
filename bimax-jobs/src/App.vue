30efa8428f5d7e24e119d6b2eab8abf8c28bac1c
Brian <brianmichaeladero@gmail.com>
2024-01-07 22:59:46 +0300

    parent-serve point

--- bimax-jobs/src/App.vue
+++ bimax-jobs/src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link :to="{ name: 'about' }">About</router-link>
+      <router-link :to="{ name: 'jobs' }">Jobs</router-link>
+    </div>
+
+    <button @click="redirect">Redirect</button>
+    <button @click="back">Go back</button>
+    <button @click="forward">Go forward</button>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {},
+    //going back through the history
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {},
+  },
+};
+</script>
 
 <style>
 #app {
@@ -17,16 +35,40 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+#nav {
+  padding: 0;
+  border-bottom: 1px solid #2c3e50;
+  max-width: 60%;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  margin: auto;
 }
 
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  margin: 2rem;
+  /* margin-bottom: 0; */
 }
 
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: #42b983;
+  background: crimson;
+  padding: 0.4rem;
+  border: none;
+  border-radius: 10px;
+}
+button {
+  margin: 15px 10px;
+  padding: 9px;
+  border: none;
+  border-radius: 7px;
+}
+
+body {
+  max-width: 80%;
+  margin: auto;
 }
 </style>
