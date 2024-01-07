dfb138fca8db4cf5d2505f0c5d485aa8414a6290
Brian <brianmichaeladero@gmail.com>
2024-01-07 23:00:59 +0300

    About page

--- bimax-jobs/src/views/AboutView.vue
+++ bimax-jobs/src/views/AboutView.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo iusto ipsam
+      beatae corporis numquam deleniti ea suscipit ipsa? Facere, odio sed
+      adipisci voluptatem sunt error deserunt, accusantium repellendus sapiente
+      nihil minima eaque. Eius placeat pariatur esse! Est aliquid explicabo
+      necessitatibus! Temporibus voluptas vel praesentium, laudantium saepe
+      dicta necessitatibus, alias esse excepturi, sint perferendis commodi
+      suscipit voluptates quasi veritatis? Tenetur sapiente laboriosam labore
+      animi. Temporibus error reprehenderit debitis ab ex sint molestias tempora
+      optio cumque ut provident quasi necessitatibus soluta libero minus,
+      deleniti dolore officiis consequatur maxime modi! Cupiditate maxime, ea
+      blanditiis nesciunt consequatur incidunt repellendus minus, reprehenderit
+      nihil saepe eveniet.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo iusto ipsam
+      beatae corporis numquam deleniti ea suscipit ipsa? Facere, odio sed
+      adipisci voluptatem sunt error deserunt, accusantium repellendus sapiente
+      nihil minima eaque. Eius placeat pariatur esse! Est aliquid explicabo
+      necessitatibus! Temporibus voluptas vel praesentium, laudantium saepe
+      dicta necessitatibus, alias esse excepturi, sint perferendis commodi
+      suscipit voluptates quasi veritatis? Tenetur sapiente laboriosam labore
+      animi. Temporibus error reprehenderit debitis ab ex sint molestias tempora
+      optio cumque ut provident quasi necessitatibus soluta libero minus,
+      deleniti dolore officiis consequatur maxime modi! Cupiditate maxime, ea
+      blanditiis nesciunt consequatur incidunt repellendus minus, reprehenderit
+      nihil saepe eveniet.
+    </p>
   </div>
 </template>
