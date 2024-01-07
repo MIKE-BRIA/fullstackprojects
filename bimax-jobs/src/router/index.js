5999d438ba2ea9c0b065b6c0aac20ad6b2381135
Brian <brianmichaeladero@gmail.com>
2024-01-07 23:00:23 +0300

    all the routers

--- bimax-jobs/src/router/index.js
+++ bimax-jobs/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
+import Jobs from "../views/Jobs/Jobs.vue";
+import JobDetails from "../views/Jobs/JobDetails.vue";
+import Errorpage from "../views/404.vue";
 
 const routes = [
   {
@@ -12,6 +15,27 @@ const routes = [
     path: "/about",
     name: "about",
     component: AboutView,
+  },
+  {
+    path: "/jobs",
+    name: "jobs",
+    component: Jobs,
+  },
+  {
+    path: "/jobs/:id",
+    name: "JobDetails",
+    component: JobDetails,
+    props: true,
+  },
+  //redirect
+  {
+    path: "/all-jobs",
+    redirect: "/jobs",
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "ErrorPage",
+    component: Errorpage,
   },
 ];
 
