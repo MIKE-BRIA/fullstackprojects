92ddfa5fd45fbd2be1c3e24d95fa563c3d74f9ce
Brian <brianmichaeladero@gmail.com>
2024-01-07 23:01:33 +0300

    Home page

--- bimax-jobs/src/views/HomeView.vue
+++ bimax-jobs/src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HomePage</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius pariatur
+      odit placeat excepturi illo unde quia alias totam nesciunt aliquam.
+      Placeat rerum, nobis reiciendis totam culpa vel sed quos est aliquam
+      quaerat iure voluptas nam repudiandae officia deleniti aperiam accusantium
+      cum repellat animi? Necessitatibus, commodi vitae eligendi voluptatum
+      laudantium quas et? Excepturi possimus quidem vel atque! Corporis
+      reiciendis incidunt voluptate maxime dignissimos alias quibusdam in hic,
+      consequatur odit? Expedita veritatis temporibus repellendus doloremque?
+      Commodi, eos. Tempore, voluptas. Dolores vitae quod delectus, accusantium
+      natus nulla commodi doloremque dolor fuga maxime expedita beatae
+      asperiores cum ratione soluta aspernatur repudiandae incidunt eaque odit.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius pariatur
+      odit placeat excepturi illo unde quia alias totam nesciunt aliquam.
+      Placeat rerum, nobis reiciendis totam culpa vel sed quos est aliquam
+      quaerat iure voluptas nam repudiandae officia deleniti aperiam accusantium
+      cum repellat animi? Necessitatibus, commodi vitae eligendi voluptatum
+      laudantium quas et? Excepturi possimus quidem vel atque! Corporis
+      reiciendis incidunt voluptate maxime dignissimos alias quibusdam in hic,
+      consequatur odit? Expedita veritatis temporibus repellendus doloremque?
+      Commodi, eos. Tempore, voluptas. Dolores vitae quod delectus, accusantium
+      natus nulla commodi doloremque dolor fuga maxime expedita beatae
+      asperiores cum ratione soluta aspernatur repudiandae incidunt eaque odit.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
