654a6c8050404ed4f681867f34fd66cccb212f65
Brian <brianmichaeladero@gmail.com>
2024-01-09 10:44:56 +0300

    post

---
+++ bimax-blog/src/components/PostList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="postlist">
+      <div v-for="post in posts" :key="post.id">
+        <SinglePost :post="post"></SinglePost>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import SinglePost from "./SinglePost.vue";
+export default {
+  name: "PostList",
+  components: { SinglePost },
+  props: ["posts"],
+  setup(props) {
+    onMounted(() => {});
+  },
+};
+</script>
+
+<style></style>
