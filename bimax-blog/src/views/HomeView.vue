799cdb4a3d13563c3048869c67d0114ef0a01b80
Brian <brianmichaeladero@gmail.com>
2024-01-09 10:45:25 +0300

    homepage

--- bimax-blog/src/views/HomeView.vue
+++ bimax-blog/src/views/HomeView.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList :posts="posts"></PostList>
+  </div>
 </template>
 
 <script>
+import PostList from "@/components/PostList.vue";
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
-  components: {},
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        title: "Welcome to the blog post",
+        body: "lorem ipsum dolor sit amet, consectetur",
+        id: 1,
+      },
+      {
+        title: "It's not the easiest thing to do",
+        body: "lorem ipsum dolor sit amet",
+        id: 2,
+      },
+      {
+        title: "Just another day looking for meaning",
+        body: "lorem ipsum dolor sit amet",
+        id: 3,
+      },
+    ]);
+
+    return { posts };
+  },
 };
 </script>
