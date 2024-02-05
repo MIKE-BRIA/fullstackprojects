18d7676d844c152818f94087b164f5275f093c32
Brian <brianmichaeladero@gmail.com>
2024-02-05 21:04:29 +0300

    all admin routes

--- ecomshop/routes/admin.routes.js
+++ ecomshop/routes/admin.routes.js
@@ -1,6 +1,3 @@
-// * routes for all admin routes
-
-//import express for use in routers
 const express = require("express");
 
 const adminController = require("../controllers/admin.controller");
@@ -8,18 +5,17 @@ const imageUploadMiddleware = require("../middlewares/image-upload");
 
 const router = express.Router();
 
-//get route for product
-router.get("/products", adminController.getProducts); // ? /admin/products
+router.get("/products", adminController.getProducts); // /admin/products
 
-router.get("/products/new", adminController.getNewProduct); // ? /admin/products/new
+router.get("/products/new", adminController.getNewProduct);
 
 router.post(
   "/products",
   imageUploadMiddleware,
   adminController.createNewProduct
-); //? extraction of uploaded image
+);
 
-router.get("/products/:id", adminController.getUpateProduct);
+router.get("/products/:id", adminController.getUpdateProduct);
 
 router.post(
   "/products/:id",
@@ -28,5 +24,9 @@ router.post(
 );
 
 router.delete("/products/:id", adminController.deleteProduct);
+
+router.get("/orders", adminController.getOrders);
+
+router.patch("/orders/:id", adminController.updateOrder);
 
 module.exports = router;
