d977c9dc8fa2db2589b05abfd02ad6f03f0b759f
Brian Michael <brianmichaeladero@gmail.com>
2024-02-22 17:26:44 +0300

    session config

--- devblog/app.js
+++ devblog/app.js
@@ -1,24 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const session = require("express-session");
+const express = require("express");
 
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
+const SessionConfig = require("./config/session");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+const sessionConfig = SessionConfig();
+app.use(session(sessionConfig));
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
-  app.listen(3000);
+  app.listen(3001);
 });
