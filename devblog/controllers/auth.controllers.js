b8ee83633dc58ab6e038f3286d4f83de28071ac6
Brian Michael <brianmichaeladero@gmail.com>
2024-02-24 20:37:43 +0300

    functions for authentification

--- devblog/controllers/auth.controllers.js
+++ devblog/controllers/auth.controllers.js
@@ -14,35 +14,35 @@ async function signup(req, res, next) {
   const trimpassword = password.trim();
 
   if (email !== confirmemail || password.length < 6 || !email.includes("@")) {
-    return res.status(403).render("signup");
+    return res.render("/signup");
   }
 
   //*user exists
   const existingUser = await db
     .getDb()
-    .collection("users")
+    .collection("user")
     .findOne({ email: email });
 
   if (existingUser) {
     console.log("user already exists");
-    return res.status(403).redirect("signup");
+    return res.redirect("/signup");
   }
 
   //*hass password
-  const hasspassword = await bcrypt.hash(trimpassword, 12);
+  const hashpassword = await bcrypt.hash(trimpassword, 12);
 
   //*new user
   const user = {
     fullname: fullname,
     email: email,
-    password: hasspassword,
+    password: hashpassword,
   };
 
   //*store user data
 
   await db.getDb().collection("user").insertOne(user);
 
-  res.redirect("login");
+  res.redirect("/login");
 }
 
 //!login post function
@@ -60,7 +60,7 @@ async function login(req, res) {
 
   if (!existingUser) {
     console.log("User not found");
-    return res.status(403).redirect("login");
+    return res.redirect("/login");
   }
 
   //*compare entered password
@@ -68,16 +68,24 @@ async function login(req, res) {
 
   if (!comparePassword) {
     console.log("You enterd wrong password");
-    return res.redirect("login");
+    return res.redirect("/login");
   }
 
-  //*storing userdata in a session
-  req.session.user = { id: existingUser._id, email: existingUser.email };
-  req.session.isAuth = true;
+  req.session.user = {
+    id: existingUser._id,
+    email: existingUser.email,
+  };
+  req.session.isAuthenticated = true;
+
+  console.log(req.session);
+
+  //writing the session to the database
   req.session.save(() => {
-    res.redirect("/");
+    res.redirect("/"); //save the session then redirect user to admin page
   });
 }
+
+// console.log("User is authenticated");
 
 module.exports = {
   signup: signup,
