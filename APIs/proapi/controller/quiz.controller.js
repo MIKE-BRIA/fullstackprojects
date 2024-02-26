3b16caf6f1012685bc54d153ba621805a853695b
Brian Michael <brianmichaeladero@gmail.com>
2024-02-26 16:10:53 +0300

    quiz api

---
+++ APIs/proapi/controller/quiz.controller.js
@@ -0,0 +1,14 @@
+function getAllQuiz(req, res, next) {}
+
+function addQuiz(req, res, next) {}
+
+function deleteQuiz(req, res, next) {}
+
+function updateQuiz(req, res, next) {}
+
+module.exports = {
+  getAllQuiz: getAllQuiz,
+  addQuiz: addQuiz,
+  deleteQuiz: deleteQuiz,
+  updateQuiz: updateQuiz,
+};
---
+++ APIs/proapi/model/quiz.model.js
@@ -0,0 +1,50 @@
+const db = require("../data/database");
+const mongodb = require("mongodb");
+
+//!Quiz class
+class Quiz {
+  constructor(text, answer, id) {
+    this.text = text;
+    this.answer = answer;
+    this.id = id;
+  }
+
+  static async getAllQuiz() {
+    const quizDocuments = await db.getDb().collection("quiz").find().toArray();
+
+    return quizDocuments.map((quizDocument) => {
+      return new Quiz(quizDocument._id, quizDocument.text, quizDocument.answer);
+    });
+  }
+
+  delete() {
+    if (!this.id) {
+      throw new Error("trying to delete a quiz that doesn't exist");
+    }
+    const quizId = new mongodb.ObjectId(this.id);
+
+    return db.getDb().collection("quiz").deleteOne({ _id: quizId });
+  }
+
+  //*saving a new quiz and an existing one
+  save() {
+    if (this.id) {
+      //turning the id into a mongodb object id
+      const quizId = new mongodb.ObjectId(this.id);
+      return db
+        .getDb()
+        .collection("quiz")
+        .updateOne(
+          { _id: quizId },
+          { $set: { text: this.text, answer: this.answer } }
+        );
+    } else {
+      return db
+        .getDb()
+        .collection("quiz")
+        .insertOne({ text: this.text, answer: this.answer });
+    }
+  }
+}
+
+module.exports = Quiz;
