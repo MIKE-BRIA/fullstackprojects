c19ecd4a844e08019e988d9323a2a4bb2d8d40a9
Brian Michael <brianmichaeladero@gmail.com>
2024-02-27 11:02:59 +0300

    server

--- APIs/proapi/app.js
+++ APIs/proapi/app.js
@@ -1,3 +1,22 @@
 const express = require("express");
-
+const db = require("./data/database");
 const app = express();
+const quizRoutes = require("./routes/quiz.routes");
+
+app.use(express.json());
+
+app.use("/quiz", quizRoutes);
+
+app.use((req, res, error, next) => {
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
+
+db.initDb()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log("connection to the databse failed");
+  });
